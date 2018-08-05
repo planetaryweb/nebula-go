93313687415b836c31401b0b1d846fb00270caff
Michael Bryant <shadow53@shadow53.com>
2018-08-05 11:04:54 -0700

    Updated dependencies

--- vendor/github.com/Shadow53/interparser/parse/time.go
+++ vendor/github.com/Shadow53/interparser/parse/time.go
@@ -1,21 +1,21 @@
 package parse
 
 import (
-    "time"
+	"time"
 )
 
 // TimeFromMilliseconds takes an interface{} that is an integer representing
 // Unix/epoch time in milliseconds and converts it to a time.Time
 func TimeFromMilliseconds(data interface{}) (time.Time, error) {
-    i, err := Int64(data)
-    if err != nil {
-        return time.Time{}, err
-    }
+	i, err := Int64(data)
+	if err != nil {
+		return time.Time{}, err
+	}
 
-    // Trim off milliseconds
-    sec := i / 1000
-    // Get milliseconds and convert to nanoseconds
-    nsec := (i % 1000) * 1000000
-    // Return result of time.Unix
-    return time.Unix(sec, nsec), nil
+	// Trim off milliseconds
+	sec := i / 1000
+	// Get milliseconds and convert to nanoseconds
+	nsec := (i % 1000) * 1000000
+	// Return result of time.Unix
+	return time.Unix(sec, nsec), nil
 }
